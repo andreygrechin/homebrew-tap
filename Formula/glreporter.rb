4b0e64c450287caf0ad75330e6f69c0975ed8af7
Andrey Grechin <36399892+andreygrechin@users.noreply.github.com>
2025-06-29 16:47:03 +0400

    fix: make it work for private repos

--- Formula/glreporter.rb
+++ Formula/glreporter.rb
@@ -9,7 +9,11 @@ class Glreporter < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/andreygrechin/glreporter/releases/download/v0.1.0/glreporter_0.1.0_darwin_x86_64.tar.gz"
+      url "https://github.com/andreygrechin/glreporter/releases/download/v0.1.0/glreporter_0.1.0_darwin_x86_64.tar.gz",
+        headers: [
+          "Accept: application/octet-stream",
+          "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
+        ]
       sha256 "a5312aa3b7a48394a27991108814e063bbd1820c2c1c699798fe7185d713273e"
 
       def install
@@ -17,7 +21,11 @@ class Glreporter < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/andreygrechin/glreporter/releases/download/v0.1.0/glreporter_0.1.0_darwin_arm64.tar.gz"
+      url "https://github.com/andreygrechin/glreporter/releases/download/v0.1.0/glreporter_0.1.0_darwin_arm64.tar.gz",
+        headers: [
+          "Accept: application/octet-stream",
+          "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
+        ]
       sha256 "581432dac24b77581a63dc2d8c752bd430bad213708ae367418c357cdaa271af"
 
       def install
@@ -28,7 +36,11 @@ class Glreporter < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/andreygrechin/glreporter/releases/download/v0.1.0/glreporter_0.1.0_linux_x86_64.tar.gz"
+      url "https://github.com/andreygrechin/glreporter/releases/download/v0.1.0/glreporter_0.1.0_linux_x86_64.tar.gz",
+        headers: [
+          "Accept: application/octet-stream",
+          "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
+        ]
       sha256 "5bba9a475e70f07ab226cddd582e1120cae2af5a752c5c6d9f87878ca91d61c5"
 
       def install
