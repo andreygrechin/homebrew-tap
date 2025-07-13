bd877de2a5a76430290c32fab5e72f2d2f934680
Andrey Grechin <36399892+andreygrechin@users.noreply.github.com>
2025-07-13 18:59:13 +0400

    fix: update brew formula for glreporter 0.4.0

--- Formula/glreporter.rb
+++ Formula/glreporter.rb
@@ -4,21 +4,21 @@
 class Glreporter < Formula
   desc "CLI tool for validating, comparing, and manipulating semantic versions"
   homepage "https://github.com/andreygrechin/glreporter"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/andreygrechin/glreporter/releases/download/v0.3.0/glreporter_0.3.0_darwin_x86_64.tar.gz"
-      sha256 "d1ef6e4979ffe3092c4c30998690070c21ae94bd40bb678834a4e0d8d3ee3f4c"
+      url "https://github.com/andreygrechin/glreporter/releases/download/v0.4.0/glreporter_0.4.0_darwin_x86_64.tar.gz"
+      sha256 "7119901a44b556ae1a89171237f8aac542510998cb2161795523cf754adf76fe"
 
       def install
         bin.install "glreporter"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/andreygrechin/glreporter/releases/download/v0.3.0/glreporter_0.3.0_darwin_arm64.tar.gz"
-      sha256 "d315d73165540b22803c09b9b4610a23044019b8af421cd93cf86bd99fa2ec8a"
+      url "https://github.com/andreygrechin/glreporter/releases/download/v0.4.0/glreporter_0.4.0_darwin_arm64.tar.gz"
+      sha256 "3f8338ecd8aef036a73fdd7feb1cf17bddbcbfbf230bbcc4d6e246e112d2acfe"
 
       def install
         bin.install "glreporter"
@@ -28,8 +28,8 @@ class Glreporter < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/andreygrechin/glreporter/releases/download/v0.3.0/glreporter_0.3.0_linux_x86_64.tar.gz"
-      sha256 "a6c0fb2dd844afe5002e498c8c8932e182ce4b8839bf95f66a5f1f6fee10cbe4"
+      url "https://github.com/andreygrechin/glreporter/releases/download/v0.4.0/glreporter_0.4.0_linux_x86_64.tar.gz"
+      sha256 "5f921374d7a279053c1c34d363790a0848a5d9cb8cac110a8555d88256fbe49a"
 
       def install
         bin.install "glreporter"
